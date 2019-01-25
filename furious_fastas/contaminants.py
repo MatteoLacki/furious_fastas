fd72ef3261898c121d26c3690510693c449e6c83
matteo <matteo.lacki@gmail.com>
2019-01-25 17:17:20 +0100

    changing the source of data

--- furious_fastas/contaminants.py
+++ furious_fastas/contaminants.py
@@ -1,15 +1,18 @@
 from os.path import join as pjoin, abspath, dirname
 
 from .fastas import Fastas
+from .tenzer_contaminants import tenzer_contaminants
+from .parse.fastas import parse
 
 def get_tenzer_contaminants():
     """Get the frequently occurring contaminants.
 
     These reflect the state of hygienne of people in Stefan Tenzer's lab, mostly Rubens'."""
-    path = pjoin(abspath(dirname(__file__)),
-                 "data/contaminants.fasta")
-    conts = Fastas()
-    conts.read(path)
-    return conts
+    # path = pjoin(abspath(dirname(__file__)),
+    #              "data/contaminants.fasta")
+    # conts.read(path)
+    # return conts
+    return Fastas(parse(tenzer_contaminants))
+    
 
 conts = get_tenzer_contaminants()
