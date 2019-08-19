4c9147d387e9635d267f839251d166971fbd0605
matteo <matteo.lacki@gmail.com>
2019-08-19 16:20:59 +0200

    Now parsing multiple urls per species (for mixes of proteomes, for instance).

--- furious_fastas/update.py
+++ furious_fastas/update.py
@@ -2,9 +2,10 @@ from pathlib import Path
 from shutil import move as mv
 from datetime import datetime
 
-from .time_ops import datestr2date, now
-from .fastas import fastas, Fastas
 from .contaminants import contaminants
+from .download import download
+from .fastas import Fastas
+from .time_ops import datestr2date, now
 
 
 def update_plgs_peaks_fastas(db_path, species2url, verbose=True):
@@ -32,15 +33,25 @@ def update_plgs_peaks_fastas(db_path, species2url, verbose=True):
     latest_NOW_PEAKS.mkdir(exist_ok=True, parents=True)
     latest_NOW_PLGS.mkdir(exist_ok=True, parents=True)
 
-    for name, url in species2url:
+    # avoid multiple downloads of the same files
+    species2url = dict(species2url)
+    if verbose:
+        print("Downloading files.")
+    url2raw = {url for urls in species2url.values() for url in urls}
+    url2raw = {url:download(url) for url in url2raw}
+
+    for name, urls in species2url.items():
         if verbose:
             print("\tUpdating {}.".format(name))
-        fs = fastas(url)
+        fs = Fastas()
+        for url in urls:
+            fs.parse(url2raw[url])
         file = "{}_{}_conts_{}_{}.fasta".format(name, str(len(fs)), str(len(contaminants)), NOW)
         fs.extend(contaminants)
         fs.write(latest_NOW_PEAKS/file)
         fs = Fastas(f.to_ncbi_general() for f in fs)
         fs.reverse()
         fs.write(latest_NOW_PLGS/file)
+    
     if verbose:
         print("Succeeeded!")
